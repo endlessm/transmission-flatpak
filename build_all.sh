9562948c3cab2a96a9a307e9b4cc70a854df8d3c
Will Thompson <wjt@endlessm.com>
2017-03-17 18:40:14 +0000

    Remove build_{all,update_repo}.sh

--- build_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for i in *.json; do
-    ./build.sh $i
-done
-
-./build_update_repo.sh
--- build_update_repo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-flatpak build-update-repo --generate-static-deltas --prune --prune-depth=8  ${EXPORT_ARGS-} repo
