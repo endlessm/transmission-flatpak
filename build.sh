3485815027bb09e09ade72d0a9a4264c94debdfb
Will Thompson <wjt@endlessm.com>
2017-03-22 16:21:35 +0000

    build.sh: update repo after build

--- build.sh
+++ build.sh
@@ -7,12 +7,16 @@ then
     ostree  init --mode=archive-z2 --repo=repo
 fi
 
+REPO=repo
+
 flatpak-builder \
     --force-clean \
     --ccache \
     --require-changes \
-    --repo=repo \
+    --repo=$REPO \
     --arch=$(flatpak --default-arch) \
     --subject="build of com.transmissionbt.Transmission, $(date)" \
     build \
     com.transmissionbt.Transmission.json
+flatpak build-update-repo \
+    $REPO
